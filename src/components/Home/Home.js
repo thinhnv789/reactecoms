e456f3ef31a8126c58d56329bc863e6cb66466d5
thinhnv789 <thinhnv.vnu@gmail.com>
2017-08-20 11:45:52 +0700

    header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,13 @@ import { Link, withRouter } from 'react-router-dom';
 import './Home.css';
 
 class  Home extends Component {
-    
-    render () {
-        return (
-            <div>
-                <h1> Home Page </h1>
-                <Link to='/about'>About</Link>
-            </div>
-        )
-    }
+  render () {
+      return (
+          <div>
+              <h1> Home Page </h1>
+          </div>
+      )
+  }
 }
 
 export default withRouter(Home)
--- src/components/Layouts/Header/Header.js
+++ src/components/Layouts/Header/Header.js
@@ -4,25 +4,53 @@ import { Link, withRouter } from 'react-router-dom'
 import './Header.css'
 
 class Header extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      menuShow: false
+    }
+  }
+
+  setToggleMenu = () => {
+    this.setState({
+      menuShow: !this.state.menuShow
+    })
+  }
+
+  navToPage = () => {
+    this.setState({
+      menuShow: false
+    })
+  }
+  
   render () {
     const { pathname } = this.props.location;
+    const { menuShow } = this.state;
 
     return (
 			<header>
         <div className="top-header">
+          <button className="navbar-toggle" onClick={this.setToggleMenu}>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
         </div>
-        <nav className="header-menu">
+        <nav className={`header-menu ${menuShow ? 'toggle' : 'collapse'}`}>
           <div className="container">
             <ul className="nav navbar">
-              <li className={ pathname==='/' ? 'active' : null}>
+              <li className={ pathname==='/' ? 'active' : null} onClick={this.navToPage}>
                 <Link to='/'>Home</Link>
               </li>
-              <li className={ pathname==='/about' ? 'active' : null}>
+              <li className={ pathname==='/about' ? 'active' : null} onClick={this.navToPage}>
                 <Link to='/about'>About</Link>
               </li>
             </ul>
           </div>
         </nav>
+        {
+          menuShow && <div className="shadow-layer" />
+        }
 			</header>
     )
   }
